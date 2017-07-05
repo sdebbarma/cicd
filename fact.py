ded73c13be1f24aa5cc3dc45cb288b2ea2ad04bd
sdebbarma <sourav.debbarma@gmail.com>
2017-07-05 16:04:21 +0530

    first time

---
+++ fact.py
@@ -0,0 +1,14 @@
+import sys
+
+def fact(n):
+	if n == 0:
+		return 1
+	return n * fact(n -1)
+
+def main(n):
+	res = fact(n)
+	print(res)
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		main(int(sys.argv[1]))
---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from fact import fact
+
+class TestFactorial(unittest.TestCase):
+    def test_fact(self):
+        res = fact(5)
+        self.assertEqual(res, 120)
+
+
+if __name__ == '__main__':
+    unittest.main()
